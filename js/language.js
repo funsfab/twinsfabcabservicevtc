c40790ee12b68671278e2cba7cde124401139122
funsfab <funsfab@gmail.com>
2026-08-10 19:10:25 +0200

    language.js

--- js/language.js
+++ js/language.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const languageButtons = document.querySelectorAll(".language-option");
 
+  languageButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      languageButtons.forEach((btn) => {
+        btn.classList.remove("is-active");
+      });
+
+      button.classList.add("is-active");
+
+      const selectedLanguage = button.dataset.lang;
+
+      console.log("Selected language:", selectedLanguage);
+    });
+  });
+});
